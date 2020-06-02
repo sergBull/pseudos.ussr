e5c8389c27656aafc401a8b1045675be163c7266
sergBull <bullsergv@gmail.com>
2020-06-02 18:16:04 +0300

    Update JASM.js

--- JASM.js
+++ JASM.js
@@ -41,6 +41,10 @@ function getDataFromMemSctor(dn) {
 	MemSector.getItem(dn);
 }
 
+function getKeyFromMemSector(name) {
+	return Object.keys(MemSector);
+}
+
 function parseCode(text) {
 	var cmds = text.split("\n");
 	
@@ -53,10 +57,24 @@ function parseCode(text) {
 		var whitespace = /\s+|\t+|\v+/;
 		var word = /[a-zA-Z][0-9]/;
 		
-		var obj = cmds[i].split(whitespace);
+		var obj = cmds[i].split("");
 		
-		if (obj[0].expr(label)) {
+		if (label.expr(obj[0])) {
 			createLabel(label.replace(":", ""));
+		}
+		
+		if (word.expr(obj[0])) {
+			switch (obj[0]) {
+				case "mov": {
+					var a = getKeyFromMemSection(word.expr(obj[1]));
+					var b = getDataFromMemSection(word.expr(obj[2]));
+					
+					deleteDataFromMemSector(a);
+					addDataToMemSector(a, b);
+					
+					alert(a);
+				}
+			}
 		}
 	}
 }
